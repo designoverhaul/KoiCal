28861eee10acde782c8ddea777a8c408822202f4
designoverhaul <aaronheine@gmail.com>
2024-11-14 14:47:28 -0500

    Initial Commit

---
+++ Koi Cal/Koi_CalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Koi_CalApp.swift
+//  Koi Cal
+//
+//  Created by Aaron Heine on 11/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Koi_CalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
