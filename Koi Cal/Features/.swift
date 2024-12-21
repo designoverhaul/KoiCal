400a0a7563e9ecf4e3fb5a255f2d94e0006d5111
designoverhaul <aaronheine@gmail.com>
2024-12-21 11:29:20 -0500

    location ont required

---
+++ Koi Cal/Features/.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Channel Lab TV
+//
+//  Created by Aaron Heine on 11/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationView {
+            ChannelGridView()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
